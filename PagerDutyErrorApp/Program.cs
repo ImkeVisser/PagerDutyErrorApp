8df9cfb88451ee141550407a27430156432dd12a
Kirsten Jalvingh <kjalvingh@conclusion.nl>
2020-07-24 18:20:56 +0200

    cof-logging (nog niet helemaal af)

--- PagerDutyErrorApp/Program.cs
+++ PagerDutyErrorApp/Program.cs
@@ -13,62 +13,56 @@ namespace PagerDutyErrorApp
     {
         static void Main(string[] args)
         {
+            TestLogger testLogger = new TestLogger();
+
+
             // Display title as the C# console calculator app.
             Console.WriteLine("Console PagerDutyError\r");
             Console.WriteLine("------------------------\n");
-
-
-
             // Ask the user to choose an option.
-            Console.WriteLine("Choose an option from the following list:");
-            Console.WriteLine("\t1 - logregel - niet interessant");
-            Console.WriteLine("\t2 - Low");
-            Console.WriteLine("\t3 - Medium");
-            Console.WriteLine("\t4 - High");
-            Console.WriteLine("\t5 - Urgent");
-            Console.Write("Your option? ");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("\t1 - log info");
+            Console.WriteLine("\t2 - log warning");
+            Console.WriteLine("\t3 - log error");
+            Console.WriteLine("\t4 - stop logging");
+            Console.WriteLine("\tq - to quit");
+            TickTock(testLogger);
+        }
 
-
-
-            // Use a switch statement to do the math.
-            switch (Console.ReadLine())
+        private static void TickTock(TestLogger testLogger)
+        {
+            bool on = true;
+            while (on)
             {
-                case "1":
-                    Console.WriteLine($"Niet interessant");
-                    break;
-                case "2":
-                    TickTock(5000);
-                    break;
-                case "3":
-                    TickTock(2000);
-                    break;
-                case "4":
-                    TickTock(500);
-                    break;
-                case "5":
-                    TickTock(5);
-                    break;
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("logging at info level now");
+                        testLogger.setLogging(LogLevel.Info);
+                        break;
+                    case "2":
+                        Console.WriteLine("logging at warning level now");
+                        testLogger.setLogging(LogLevel.Warning);
+                        break;
+                    case "3":
+                        Console.WriteLine("logging at error level now");
+                        testLogger.setLogging(LogLevel.Error);
+                        break;
+                    case "4":
+                        Console.WriteLine("no logging");
+                        testLogger.setLogging(LogLevel.None);
+                        break;
+                    case "q":
+                        on = false;
+                        break;
+                    default:
+                        Console.WriteLine("input not valid");
+                        break;
+                }
             }
-            // Wait for the user to respond before closing.
-            Console.Write("Press any key to close PagerDutyError...");
-            Console.ReadKey();
         }
 
 
 
-        public static void TickTock(int interval)
-        {
-            Timer aTimer = new Timer();
-            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-            aTimer.Interval = interval;
-            aTimer.Enabled = true;
-            Console.WriteLine("Press \'q\' to quit the sample.");
-            while (Console.Read() != 'q') ;
-        }
-        // Specify what you want to happen when the Elapsed event is raised.
-        private static void OnTimedEvent(object source, ElapsedEventArgs e)
-        {
-            Console.WriteLine("Groen van buiten, Rood van binnen.... Watermeloen");
-        }
     }
 }
---
+++ PagerDutyErrorApp/TestLogger.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using Fh.Cof.Csl.Logging;
+
+
+namespace PagerDutyErrorApp
+{
+    enum LogLevel
+    {
+        Error,
+        Warning,
+        Info,
+        None
+    }
+    class TestLogger
+    {
+        private string errorLog = "Dit is een error log vanuit de test-app";
+        private string warningLog = "Dit is een info log vanuit de test-app";
+        private string infoLog = "Dit is een warning log vanuit de test-app";
+        private LogLevel currentLogLevel = LogLevel.None;
+        private Timer timer = new Timer(2000);
+
+        public void changeInterval(int interval)
+        {
+            timer.Interval = interval;
+        }
+
+        public void setLogging(LogLevel level)
+        {
+            currentLogLevel = level;
+            timer.Enabled = false;
+            switch (level)
+            {
+                case LogLevel.Error:
+                    timer.Elapsed += new ElapsedEventHandler(LogError);
+                    break;
+                case LogLevel.Warning:
+                    timer.Elapsed += new ElapsedEventHandler(LogWarning);
+                    break;
+                case LogLevel.Info:
+                    timer.Elapsed += new ElapsedEventHandler(LogInfo);
+                    break;
+                case LogLevel.None:
+                    stopLogging();
+                    break;                   
+            }           
+        }
+
+        private void stopLogging()
+        {
+            switch(currentLogLevel)
+                {
+                case LogLevel.Error:
+                    timer.Elapsed -= LogError;
+                    break;
+                case LogLevel.Warning:
+                    timer.Elapsed -= LogWarning;
+                    break;
+                case LogLevel.Info:
+                    timer.Elapsed -= LogInfo;
+                    break;
+                }
+            currentLogLevel = LogLevel.None;
+            timer.Enabled = false;
+        }
+
+        private void LogError(object source, ElapsedEventArgs e)
+        {
+            currentLogLevel = LogLevel.Error;
+            timer.Enabled = true;
+            if (currentLogLevel.Equals(LogLevel.Error))
+            {
+                Console.WriteLine("error");
+                //FhLogging.LogError(errorLog);
+            }
+        }
+
+        private void LogWarning(object source, ElapsedEventArgs e)
+        {
+            currentLogLevel = LogLevel.Warning;
+            timer.Enabled = true;
+            if (currentLogLevel.Equals(LogLevel.Warning))
+            {
+                Console.WriteLine("warning");
+                //FhLogging.LogError(warningLog);
+            }
+        }
+
+        private void LogInfo(object source, ElapsedEventArgs e)
+        {
+            currentLogLevel = LogLevel.Info;
+            timer.Enabled = true;
+            if (currentLogLevel.Equals(LogLevel.Info))
+            {
+                Console.WriteLine("info");
+                //FhLogging.LogError(infoLog);
+            }
+        }
+
+    }
+}
